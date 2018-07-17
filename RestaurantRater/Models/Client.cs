23bfe875da709123ad03df1afc978d745a75a682
ehenderson421 <ehenderson421@gmail.com>
2018-07-17 14:44:21 -0500

    first push

---
+++ RestaurantRater/Models/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Client
+    {
+        [Key]
+        public int ClientId { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+    }
+}
---
+++ RestaurantRater/Models/Restaurant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantId { get; set; }
+
+        [Display(Name = "Name Of Restaurant")]
+        public string RestaurantName { get; set; }
+
+        [Display(Name = "Cuisine")]
+        public string Cuisine { get; set; }
+
+        [Display(Name = "TotalRating")]
+        public string TotalRating { get; set; }
+
+
+    }
+}
